1c1eb5f3a095426700631eda32769af538ac6798
gabriel.ibagon <gabriel.ibagon@gmail.com>
2016-07-02 12:34:08 -0400

    first commit

---
+++ filters.py
@@ -0,0 +1,77 @@
+import numpy as np
+from scipy import signal
+import csv
+import matplotlib.pyplot as plt
+from io import BytesIO
+
+# np.set_printoptions(threshold=np.nan)
+
+class Filter_Viewer:
+  def __init__(self):
+    self.fs = 250           # Sampling frequency
+    self.fn = self.fs/2          # Nyquist frequency
+    print("cool")
+
+  # DATA IMPORT
+  def data_import(self,filename,delimiter=None,R1=1,R2=None,C1=1,C2=None):
+    eeg_data = np.genfromtxt(filename,delimiter=delimiter,dtype='float32')[0:]
+    if R2 is None:
+      R2 = len(eeg_data)
+    if C2 is None:
+      C2 = len(eeg_data[0])
+    return eeg_data[(R1-1):(R2+1),(C1-1):(C2+1)]
+
+  def filters(self,eeg_data):
+    #FILTER CONSTANTS
+    fs = self.fs
+    fn = self.fn
+    filter_order = 2   #2nd order filter
+    f_high = 50
+    f_low = 5
+    wn = [59,61]       #Nyquist filter window
+
+    [b,a] = signal.butter(filter_order,f_high/fn, 'low')
+    [b1,a1] = signal.butter(filter_order,f_low/fn, 'high')
+    [bn,an] = signal.butter(4,[x/fn for x in wn], 'stop')
+
+    filtered_eeg = []
+    spectogram = []
+    notched = []
+    high_passed = []
+    low_passed = []
+    print(eeg_data)
+    for i in range(len(eeg_data[0])):
+      channel =  eeg_data[:,i]
+      high_passed = signal.filtfilt(b1,a1,channel);        # high pass filter
+      low_passed = signal.filtfilt(b,a,high_passed);                # low pass filter
+      y = signal.filtfilt(bn,an,low_passed);        # notch filter
+      filtered_eeg.append(y);
+    print(filtered_eeg)
+    return filtered_eeg
+
+  def plot(self,filtered_eeg):
+    x_low = 7000
+    x_high = 8000
+    y_low = -200
+    y_high = 200
+
+    x = np.linspace(0,len(filtered_eeg[0]), num=len(filtered_eeg[0]), dtype=int)
+    plt.figure(1)
+    for channel in filtered_eeg:
+      plt.subplot(311)
+      plt.plot(x,channel)
+      plt.axis([x_low,x_high,y_low,y_high])
+    spec1 = plt.subplot(312)
+    plt.specgram(filtered_eeg[0],Fs=250)
+    spec2 = plt.subplot(313)
+    plt.specgram(filtered_eeg[1],Fs=250)
+    plt.show()
+
+def main():
+  fv = Filter_Viewer()
+  data = fv.data_import('wangshu.txt.csv',delimiter=',')
+  print('data', data)
+  filtered_data = fv.filters(data)
+  fv.plot(filtered_data)
+if __name__ == '__main__':
+  main()
