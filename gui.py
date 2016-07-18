6a1909db2c3a042a5fcb9362a529744d094e5a55
gabrielibagon <gabriel.ibagon@gmail.com>
2016-07-18 18:08:11 -0400

    added file passing btwn widgets, started main window

--- gui.py
+++ gui.py
@@ -12,49 +12,51 @@ import signal
 
 
 
-class MyMplCanvas(FigureCanvas):
+class MplCanvas(FigureCanvas):
   def __init__(self, parent=None, width=5, height=4, dpi=100):
       fig = Figure(figsize=(width, height), dpi=dpi)
-      # self.axes = fig.add_subplot(111)
-      # self.axes.hold(False)
       FigureCanvas.__init__(self, fig)
       self.setParent(parent)
-
-      # FigureCanvas.setSizePolicy(self,
-      #                            QtGui.QSizePolicy.Expanding,
-      #                            QtGui.QSizePolicy.Expanding)
-      # FigureCanvas.updateGeometry(self)
-  #     self.compute_initial_figure()
-
-  
-  # def compute_initial_figure(self):
-  #   filtered_eeg = data.get_data()
-  #   x = linspace(0,len(filtered_eeg[0]), 
-  #                   num=len(filtered_eeg[0]), dtype=int)
-  #   y = filtered_eeg[0]
-  #   self.axes.plot(x,y)
   
   def update_figure():
     pass
 
-class FilterCanvas():
+class FilterCanvas(QtGui.QWidget):
   def __init__(self, parent=None, width=5, height=4, dpi=100):
-      fig = Figure(figsize=(width, height), dpi=dpi)
-      self.axes = fig.add_subplot(111)
-      self.axes.hold(False)
-      FigureCanvas.__init__(self, fig)
+      QtGui.QWidget.__init__(self)
       self.setParent(parent)
 
-      FigureCanvas.setSizePolicy(self,
-                                 QtGui.QSizePolicy.Expanding,
-                                 QtGui.QSizePolicy.Expanding)
-      FigureCanvas.updateGeometry(self)
+      self.layout = QtGui.QGridLayout()
+      self.setLayout(self.layout)
+
+
+
+      self.file_button = QtGui.QPushButton("File", self)
+      self.file_button.setFixedWidth(75)
+
+
+      file_label = QtGui.QLabel()
+      file_label.setText("File:")
+      file_label.setStyleSheet('font-weight:bold')
+
+      self.file_name_label = QtGui.QLabel()
+
+      delim_label = QtGui.QLabel()
+      delim_label.setText("Delimiter")
+
+      self.layout.addWidget(self.file_button,1,0)
+      self.layout.addWidget(file_label,2,0)
+      self.layout.addWidget(self.file_name_label,2,1)
+      self.layout.addWidget(delim_label,3,0)
+
+
 
 class FileWindow(QtGui.QWidget):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
       QtGui.QWidget.__init__(self)
       self.setWindowTitle("File Import")
-      self.file_path = ""
+      self.file_path = ''
+      self.local_file_path = ""
       self.file_name = "None"
       self.layout = QtGui.QGridLayout()
       self.setLayout(self.layout)
@@ -63,20 +65,24 @@ class FileWindow(QtGui.QWidget):
       window_label.setText("File Import")
       window_label.setStyleSheet('font-size: 16pt; font-weight:bold')
 
-
       self.file_button = QtGui.QPushButton("File", self)
       self.file_button.setFixedWidth(75)
       self.file_button.clicked.connect(self.select_file)
-      
-      self.file_label = QtGui.QLabel()
-      self.file_label.setText(self.file_name)
 
+
+      file_label = QtGui.QLabel()
+      file_label.setText("File:")
+      file_label.setStyleSheet('font-weight:bold')
+
+      self.file_name_label = QtGui.QLabel()
+      self.file_name_label.setText(self.file_name)
 
       delim_label = QtGui.QLabel()
       delim_label.setText("Delimiter")
 
       self.delim_entry = QtGui.QLineEdit()
       self.delim_entry.setFixedWidth(40)
+      self.delim_entry.setText(',')
 
       start_row_label = QtGui.QLabel()
       start_row_label.setText("Start Row")
@@ -111,35 +117,47 @@ class FileWindow(QtGui.QWidget):
 
 
       self.layout.addWidget(window_label,0,0,1,2)
+
       self.layout.addWidget(self.file_button,1,0)
-      self.layout.addWidget(self.file_label,1,1)
-      self.layout.addWidget(delim_label,2,0)
-      self.layout.addWidget(self.delim_entry,2,1)
-      self.layout.addWidget(start_row_label,3,0)
-      self.layout.addWidget(self.start_row_entry,3,1)
-      self.layout.addWidget(end_row_label,4,0)
-      self.layout.addWidget(self.end_row_entry,4,1)
-      self.layout.addWidget(start_col_label,5,0)
-      self.layout.addWidget(self.start_col_entry,5,1)
-      self.layout.addWidget(end_col_label,6,0)
-      self.layout.addWidget(self.end_col_entry,6,1)
-      self.layout.addWidget(self.import_button,7,0,1,2)
+      self.layout.addWidget(file_label,2,0)
+      self.layout.addWidget(self.file_name_label,2,1)
+      self.layout.addWidget(delim_label,3,0)
+      self.layout.addWidget(self.delim_entry,3,1)
+      self.layout.addWidget(start_row_label,4,0)
+      self.layout.addWidget(self.start_row_entry,4,1)
+      self.layout.addWidget(end_row_label,5,0)
+      self.layout.addWidget(self.end_row_entry,5,1)
+      self.layout.addWidget(start_col_label,6,0)
+      self.layout.addWidget(self.start_col_entry,6,1)
+      self.layout.addWidget(end_col_label,7,0)
+      self.layout.addWidget(self.end_col_entry,7,1)
+      self.layout.addWidget(self.import_button,8,0,1,2)
 
     def select_file(self):
-      self.file_path = QtGui.QFileDialog.getOpenFileName()
-      self.file_name = os.path.basename(self.file_path)
-      print(self.file_path)
+      # dialog = QtGui.QFileDialog()
+      self.local_file_path = QtGui.QFileDialog.getOpenFileName()
+      self.file_name = os.path.basename(self.local_file_path)
+      print(self.local_file_path)
       print(self.file_name)
-      self.file_label.setText(self.file_name)
+      self.file_name_label.setText(self.file_name)
+
+
 
     def import_file(self):
-      #TODO
-      # check if all of the fields have been selected
-      print("FILE SET!")
-    # def create_child(self):
-    #     # here put the code that creates the new window and shows it.
-    #    child = FileWindow(self)
-    #    child.show()
+      #Errors
+      if ((self.local_file_path == '') or (self.start_row_entry.text() is '') or (self.end_row_entry.text() is '') or 
+              (self.start_col_entry.text() is '') or (self.end_col_entry.text() is '')) :
+        print('empty')
+        return 
+      if (int(self.start_row_entry.text()) > int(self.end_row_entry.text())) and (int(self.end_row_entry.text())!=-1):
+        print('start row > end row error')
+        return
+      if (int(self.start_col_entry.text()) > int(self.end_col_entry.text())) and (int(self.end_col_entry.text())!=-1):
+        print('start col > end row error')
+        return
+      self.file_path = self.local_file_path;
+
+      self.close()
 
 
 class ApplicationWindow(QtGui.QMainWindow):
@@ -162,8 +180,10 @@ class ApplicationWindow(QtGui.QMainWindow):
     self.main_widget = QtGui.QWidget(self)
 
     l = QtGui.QVBoxLayout(self.main_widget)
-    sc = MyMplCanvas(self.main_widget, width=5, height=4, dpi=100)
-    l.addWidget(sc)
+    mpl = MplCanvas(self.main_widget, width=5, height=4, dpi=100)
+    fc = FilterCanvas(self.main_widget,width=5, height=4, dpi=100)
+    l.addWidget(fc)
+    l.addWidget(mpl)
 
     self.main_widget.setFocus()
     self.setCentralWidget(self.main_widget)
@@ -172,6 +192,8 @@ class ApplicationWindow(QtGui.QMainWindow):
     self.fw = FileWindow()
     self.fw.setGeometry(QtCore.QRect(100,100,250,300))
     self.fw.show()
+    self.file_path = self.fw.file_path
+
   def fileQuit(self):
      self.close()
 
